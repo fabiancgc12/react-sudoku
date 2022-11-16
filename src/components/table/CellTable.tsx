bb1a7a5b8bf5fbb024d8b9b4a7b4f66bdeaffdef
fabian graterol <fabiancgraterol@gmail.com>
2022-11-16 15:40:13 -0400

    mostrando estilos en celda seleccionada

--- src/components/table/CellTable.tsx
+++ src/components/table/CellTable.tsx
@@ -6,16 +6,36 @@ type CellTableType = {
     cell:Cell
 }
 
+function getBackgroundColor(cell: Cell, selectedCoordinates: [column: number, row: number] | undefined) {
+    let resp = "bg-stone-800"
+
+    if (selectedCoordinates && cell.column == selectedCoordinates[0] && cell.row == selectedCoordinates[1])
+        resp = "bg-sky-900"
+    return resp
+}
+
+function getTextColor(cell: Cell, selectedCoordinates: [column: number, row: number] | undefined) {
+    let resp = "text-slate-200"
+
+    if (selectedCoordinates && cell.column == selectedCoordinates[0] && cell.row == selectedCoordinates[1])
+        resp = "text-cyan-500"
+    return resp
+}
+
 export function CellTable({cell}:CellTableType){
     const [selectedCoordinates,setSelectedCoordinates] = useAtom((selectedCoordinatesAtom))
     const onClick = () => {
         setSelectedCoordinates([cell.column,cell.row])
     }
+
+    const backGroundColor = getBackgroundColor(cell,selectedCoordinates)
+    const textColor = getTextColor(cell,selectedCoordinates)
+
     return (
     <span
         onClick={onClick}
-        className={`tableCell grid place-items-center cursor-pointer font-bold text-xl text-slate-200 bg-stone-800 border border-black
-        hover:bg-cyan-900 hover:text-cyan-500`}>
+        className={`tableCell grid place-items-center cursor-pointer font-bold text-xl ${textColor} ${backGroundColor} border border-black
+        hover:bg-sky-700 hover:text-cyan-500`}>
         {cell.column}
     </span>)
 }
