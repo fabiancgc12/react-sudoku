2d3ac5c240b3252c7ef61f88900dd021738dd091
fabian graterol <fabiancgraterol@gmail.com>
2022-11-15 23:39:34 -0400

    adding "box" property

--- src/utils/table/Cell.ts
+++ src/utils/table/Cell.ts
@@ -1,17 +1,17 @@
 export class Cell {
 
-    value:number;
+    value?:number = undefined;
     solution:number;
     row:number;
     column:number;
+    box:number
 
     constructor(value:string,solution:string,column:number,row:number) {
-        if (value == ".")
-            this.value = 0;
-        else
+        if (value != ".")
             this.value = Number(value)
         this.solution = Number(solution)
         this.row = row + 1;
         this.column = column + 1;
+        this.box = (this.row - 1)*9 + this.column
     }
 }
