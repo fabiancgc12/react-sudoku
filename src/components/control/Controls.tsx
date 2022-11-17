dbc011498b071828050c954659d274839c364ab1
fabian graterol <fabiancgraterol@gmail.com>
2022-11-17 14:04:08 -0400

    creatinf button for starting new Game

--- src/components/control/Controls.tsx
+++ src/components/control/Controls.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import {InputControl} from "@/components/control/inputs/InputControls";
 import {useAppDispatch} from "@/globalState/appStore";
 import { startGame } from "@/globalState/gameSlice/gameSlice";
+import {ActionButton} from "@/components/control/actions/ActionButton";
+import {RiRestartFill} from "react-icons/all";
 
 export function Controls(){
     const dispatch = useAppDispatch()
@@ -11,7 +13,11 @@ export function Controls(){
     return (
         <div className={'w-4/6 mt-4'}>
             <InputControl/>
-            <button onClick={onClick}>new</button>
+            <div className={"mt-4"}>
+                <ActionButton onClick={onClick} label={"New Game"}>
+                    <RiRestartFill size={30} className={"group-hover:fill-yellow-600"}/>
+                </ActionButton>
+            </div>
         </div>
     )
 }
---
+++ src/components/control/actions/ActionButton.tsx
@@ -0,0 +1,16 @@
+import {MouseEventHandler, ReactNode} from "react";
+
+type props = {
+    children:ReactNode,
+    onClick:MouseEventHandler<HTMLButtonElement>
+    label:string
+}
+
+export function ActionButton({children,onClick,label}:props){
+    return (
+        <button onClick={onClick} className={"group flex flex-col justify-center items-center bg-zinc-600 hover:bg-zinc-700 rounded-md p-2"}>
+            <span>{children}</span>
+            <span className={"group-hover:text-yellow-600"}>{label}</span>
+        </button>
+    )
+}
