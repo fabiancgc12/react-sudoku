d6b7402aa6dbae0d7d00083c57f9ace838b08d7b
fabian graterol <fabiancgraterol@gmail.com>
2022-11-28 18:36:12 -0400

    fixing mobile styles

--- src/components/control/inputs/InputButton.tsx
+++ src/components/control/inputs/InputButton.tsx
@@ -21,8 +21,8 @@ export function InputButton({value}:props){
        <button
            // @ts-ignore
            style={style}
-           className={`inputValueButton relative rounded sm:rounded-full w-9 h-11 sm:w-14 sm:h-14 bg-gray-900 text-slate-200 font-bold text-xl
-                        after:content-[''] after:absolute after:rounded sm:after:rounded-full after:-z-10 after:top-2/4 after:left-2/4 after:w-10 after:h-12 sm:after:w-16 sm:after:h-16 after:-translate-y-2/4 after:-translate-x-2/4
+           className={`inputValueButton relative rounded sm:rounded-full w-8 h-10 sm:w-14 sm:h-14 bg-gray-900 text-slate-200 font-bold text-xl
+                        after:content-[''] after:absolute after:rounded sm:after:rounded-full after:-z-10 after:top-2/4 after:left-2/4 after:w-9 after:h-11 sm:after:w-16 sm:after:h-16 after:-translate-y-2/4 after:-translate-x-2/4
                         hover:bg-gray-600`}
            onClick={onClick}
        >
--- src/components/layout/shell.tsx
+++ src/components/layout/shell.tsx
@@ -10,15 +10,13 @@ type props = {
 
 export function Shell({children}:props) {
     return (
-        <div className="flex flex-col flex-no-wrap flex-col-reverse md:flex-row min-h-screen justify-between">
+        <div className="flex flex-no-wrap md:flex-row min-h-screen justify-between">
             <div
                 className="z-20 w-full md:w-44 md:h-screen fixed bg-gray-800 shadow md:sticky md:top-0  md:flex md:flex-col justify-between
                             inset-x-0 bottom-0"
             >
                 <NavBar/>
             </div>
-            {/* Sidebar ends */}
-            {/* Remove class [ h-64 ] when adding a card block */}
             <div className="isolate container bg-zinc-900 z-10 basis-full w-full pt-6 px-4 pb-12 md:pb-5 sm:mb-0 md:py-4 md:w-4/5">
                 {children}
             </div>
