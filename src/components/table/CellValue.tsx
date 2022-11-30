4097558ab715f39c8cd372a0a30c0076d5100dbe
fabian graterol <fabiancgraterol@gmail.com>
2022-11-29 20:12:21 -0400

    changing background styles of cells

--- src/components/table/CellValue.tsx
+++ src/components/table/CellValue.tsx
@@ -2,27 +2,30 @@ import {Cell} from "@/utils/table/Cell";
 
 function getBackgroundColor(cell: Cell, selected: Awaited<Cell>) {
 
-    let resp = "bg-stone-800"
+    let resp = "bg-stone-800" // defaullt bg for cells
     const cellHasError = cell.value && cell.value != cell.solution
     const selectedHasError = selected.value && selected.value != selected.solution
     const cellIsTheSame = cell.id == selected.id
     const cellSharesColumnOrRow = cell.column == selected.column || cell.row == selected.row
     const shareBox = cell.box == selected.box
-    //checking is cell has the incorrect value
-    if (cellIsTheSame && cellHasError){
-        resp = "bg-red-900"
+    const hasTheSameValue = cell.value == selected.value
+
+    //styles if its the same cell has the selected one
+    if (cellIsTheSame){
+        if (cellHasError) resp = "bg-red-900"
+        else resp = "bg-sky-900"
     }
-    // checking
-    else if (selectedHasError && (cellSharesColumnOrRow || shareBox)){
-        resp = "bg-red-800"
+    //styles if its share column,row or box
+    else if (cellSharesColumnOrRow || shareBox){
+        if (hasTheSameValue && (selectedHasError || cellHasError)) resp = "bg-red-800"
+        else if (hasTheSameValue) resp = "bg-sky-800"
+        else resp = "bg-stone-700"
     }
-    // checking if its the same cell
-    else if (cellIsTheSame){
-        resp = "bg-sky-900"
+    //style if they share the same value
+    else if (hasTheSameValue){
+        resp = "bg-sky-800"
     }
-    // checking if they are in the same row or column
-    else if (cellSharesColumnOrRow)
-        resp = "bg-sky-900"
+
     return resp
 }
 
